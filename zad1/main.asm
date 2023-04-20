b7f177a1260f5f6f5b3bbac5b06dc5e07499cd82
xramzesx <xramzesbusiness@gmail.com>
2023-04-20 08:24:35 +0200

    [zad1] add initial parsing procedure

--- zad1/main.asm
+++ zad1/main.asm
@@ -42,6 +42,19 @@ START1:
 
 	MOV	DX, OFFSET buff + 2
 	CALL 	print
+	
+	; GET BUFFER LENGTH ;
+
+	XOR	AX, AX	; clear ax
+	XOR	CX, CX	; clear cx
+
+	MOV 	SI, OFFSET buff	; get start pointer
+	MOV	AL, [SI + 1]	; copy length value to ax
+	
+	MOV	CX, AX	; set string length
+	
+	MOV	SI, OFFSET buff + 2	; set pointer to first character
+	call	parse_input
 
 	; END PROGRAM ;
 
@@ -105,10 +118,102 @@ exit PROC
 	RET
 exit ENDP
 
+; [USAGE]:
+; 	MOV si, OFFSET source_string
+; 	MOV di, OFFSET destination_string
+; 	CALL cmp_str
+; 	<...> do with buff whatever you want <...>
+;
+cmp_str PROC
+	cmp_str_loop:
+		push 	cx
+
+		mov al, [si] ; Wczytaj znak z pierwszego stringu do rejestru AL
+		mov ah, [di] ; Wczytaj znak z drugiego stringu do rejestru AH
+		cmp al, ah ; Porównaj znaki
+
+
+		INC	SI
+		INC	DI
+
+		pop 	cx
+		loop 	cmp_str_loop
+
+	cmp_str_true:
+
+		RET
+
+	cmp_str_false:
+
+		RET
+	RET
+cmp_str ENDP
+
+
+; [USAGE]:
+; 	MOV si, OFFSET source_string
+;	MOV cx, string_length
+; 	CALL parse_input
+; 	<...> do with buff whatever you want <...>
+;
+parse_input PROC
+
+	; parse codes (at BL):
+	; 0h	- finish
+	; 1h	- parse first
+	; 2h	- parse operator
+	; 3h	- parse second
+
+	; SET INITIAL STATE ;
+	XOR	BL, BL
+	MOV	BL, 1h
+	
+	remove_spaces:
+		PUSH	CX
+		
+		; mov	dx, OFFSET t2
+		; call	print
+
+		MOV	AL, [SI]
+		CMP	AL, ' '
+		JNE	parse_first
+		; if (si != ' ') then goto parse_first
+		
+
+		INC	SI
+		POP	CX
+		LOOP	remove_spaces
+		JMP parse_finish
+		; RET
+
+	parse_hub:
+		CMP	BL, 1H
+		JE	parse_first
+
+		CMP	BL, 2H
+		JE	parse_first
+
+		CMP	BL, 3H
+		JE	parse_first
+
+		JMP parse_finish
+
+	parse_first:
+		mov dx, offset t1
+		call print
+		JMP parse_finish
+	parse_finish:
+
+		RET
+
+parse_input ENDP
+
 CODE_SEG ENDS
 
-	DW	300 DUP(?)
+;--[STACK SEGMENT]---------------------------- ;
+
 STACK_SEG SEGMENT STACK
+	DW	300 DUP(?)
 WSTOS1	DW	?
 STACK_SEG ENDS
 
